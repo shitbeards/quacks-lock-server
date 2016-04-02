0fca13b443bbc18f339571b18734c346930e8fa0
Goatus123 <oskar@spddo.co.uk>
2016-04-02 16:06:30 +0100

    port binding

--- main.go
+++ main.go
@@ -4,15 +4,20 @@ import (
 	"flag"
 	"log"
 	"net/http"
+    "os"
 )
 
 var addr = flag.String("addr", ":8081", "http service address")
 
 func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+		port = *addr
+	}
 	flag.Parse()
 	go h.run()
 	http.HandleFunc("/ws", serveWs)
-	err := http.ListenAndServe(*addr, nil)
+	err := http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
